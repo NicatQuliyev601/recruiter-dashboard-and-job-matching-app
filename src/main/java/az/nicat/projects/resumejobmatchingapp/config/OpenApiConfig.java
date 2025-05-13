98ee69139c040e2bdbb64101628cf5374c5cf3c9
NicatQuliyev601 <quliyevv.nicat2003@gmail.com>
2025-05-13 10:23:26 +0400

    Update OpenApiConfig.java

--- src/main/java/az/nicat/projects/resumejobmatchingapp/config/OpenApiConfig.java
+++ src/main/java/az/nicat/projects/resumejobmatchingapp/config/OpenApiConfig.java
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 
 @SecurityScheme(
         name = "bearerAuth",
